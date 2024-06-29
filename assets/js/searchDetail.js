b6f0cc82ea4c996cdab879a134a1d74ce0574d59
yhs99 <khb113322@naver.com>
2024-06-29 20:23:02 +0900

    카카오 마커 개선

--- assets/js/searchDetail.js
+++ assets/js/searchDetail.js
@@ -168,7 +168,6 @@ function getRoutesDefaultInfo(items) {
 }
 
 function renderDocument() {
-  //console.log(routesDefaultInfo);
   if(routesDefaultInfo.length > 5) $(".list-group").removeClass("d-flex").removeClass("justify-content-center");
   $(".badge").html(routesDefaultInfo.length + "코스");
   $("#title-travelcourse-title").html(defaultInfo.title);
@@ -218,60 +217,54 @@ function renderDocument() {
   $("#tags").html(tags);
 }
 function renderKAKAOMap() {
-  let zoom = 7;
-  let distance = parseInt(introInfo.distance);
-  if(distance > 0 && distance <= 20) {
-    zoom = 7;
-  }else if(distance > 20 && distance <= 50) {
-    zoom = 8;
-  }else if(distance > 50 && distance <= 90) {
-    zoom = 9;
-  } else {
-    zoom = 11;
-  }
-  //console.log(routesDefaultInfo);
-  let container = document.getElementById('map');
-  let options = {
-    center: new kakao.maps.LatLng(routesDefaultInfo[1].mapy, routesDefaultInfo[1].mapx), //지도의 중심좌표.
-    level: zoom //지도의 레벨(확대, 축소 정도)
-  }
+  try {
+    let container = document.getElementById('map');
+    let lines=[],positions = [];
+    let bounds = new kakao.maps.LatLngBounds();
+    let options = {
+      center: new kakao.maps.LatLng(routesDefaultInfo[1].mapy, routesDefaultInfo[1].mapx), //지도의 중심좌표.
+      level: 5 //지도의 레벨(확대, 축소 정도)
+    }
 
-  let lines=[],positions = []
-  let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-  for(let routes of routesDefaultInfo) {
-    lines.push(new kakao.maps.LatLng(routes.mapy, routes.mapx));
-    positions.push({
-      title: routes.title,
-      latlng: new kakao.maps.LatLng(routes.mapy, routes.mapx)
-    });
-  }
-  var polyline = new kakao.maps.Polyline({
-    path: lines, // 선을 구성하는 좌표배열 입니다
-    strokeWeight: 4, // 선의 두께 입니다
-    strokeColor: '#000000', // 선의 색깔입니다
-    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-    strokeStyle: 'solid' // 선의 스타일입니다
-  });
-
-  
-
-  var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-  polyline.setMap(map);
-  for (var i = 0; i < positions.length; i ++) {
-    
-    // 마커 이미지의 이미지 크기 입니다
-    var imageSize = new kakao.maps.Size(24, 35); 
-    
-    // 마커 이미지를 생성합니다    
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-    
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: positions[i].latlng, // 마커를 표시할 위치
-        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage // 마커 이미지 
+    let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+    for(let routes of routesDefaultInfo) {
+      lines.push(new kakao.maps.LatLng(routes.mapy, routes.mapx));
+      positions.push({
+        title: routes.title,
+        latlng: new kakao.maps.LatLng(routes.mapy, routes.mapx)
       });
+
+    }
+    var polyline = new kakao.maps.Polyline({
+      path: lines, // 선을 구성하는 좌표배열 입니다
+      strokeWeight: 4, // 선의 두께 입니다
+      strokeColor: '#000000', // 선의 색깔입니다
+      strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: 'solid' // 선의 스타일입니다
+    });
+
+    let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    polyline.setMap(map);
+    for (let i = 0; i < positions.length; i ++) {
+      
+      // 마커 이미지의 이미지 크기 입니다
+      let imageSize = new kakao.maps.Size(24, 35); 
+      
+      // 마커 이미지를 생성합니다    
+      let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+      
+      // 마커를 생성합니다
+      let marker = new kakao.maps.Marker({
+          map: map, // 마커를 표시할 지도
+          position: positions[i].latlng, // 마커를 표시할 위치
+          title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          image : markerImage // 마커 이미지
+        });
+      bounds.extend(lines[i]);
+    }
+    map.setBounds(bounds); // 모든 마커를 잘 보이게끔 지도를 재설정해준다.
+  }catch (err) {
+    $("#map").html("카카오 맵 로딩에 실패했습니다.");
   }
 }
 
