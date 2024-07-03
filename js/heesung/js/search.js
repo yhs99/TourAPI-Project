ad3dfa1b233051c3429bb5d9272e4fa8c649594d
yhs99 <khb113322@naver.com>
2024-07-03 16:29:32 +0900

    refactoring

--- js/heesung/js/search.js
+++ js/heesung/js/search.js
@@ -1,5 +1,5 @@
 const API_KEY = "5dx9VltlDcWscOP7NLW0yH/slO2Tl2qnffXGNS1HOhuhhi2KUrHaEzADPUbDY0bUb0zn7FprTTiQHnIRcO1psw==";
-const BASE_URL_AREACODE = `${location.protocol}//apis.data.go.kr/B551011/${localStorage.getItem("language") == "ko" ? "KorService1" : "EngService1"}/areaCode`;
+const BASE_URL_AREACODE = `${location.protocol}//apis.data.go.kr/B551011/${localStorage.getItem("language") == "ko" ? "KorService1" : "EngService1"}/areaCode1`;
 const BASE_URL_AREABASED_LIST = `${location.protocol}//apis.data.go.kr/B551011/${localStorage.getItem("language") == "ko" ? "KorService1" : "EngService1"}/areaBasedList1`;
 const BASE_URL_KEYWORD = `${location.protocol}//apis.data.go.kr/B551011/${localStorage.getItem("language") == "ko" ? "KorService1" : "EngService1"}/searchKeyword1`;
 const BASE_URL_LOCATION = `${location.protocol}//apis.data.go.kr/B551011/${localStorage.getItem("language") == "ko" ? "KorService1" : "EngService1"}/locationBasedList1`;
